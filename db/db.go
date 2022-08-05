09bf57bbbec71f6574934d69a568d952b432afa3
cchen666 <cchenlp@126.com>
2022-08-05 23:27:29 +0800

    Add Search function()

--- db/db.go
+++ db/db.go
@@ -2,15 +2,25 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 func Connect() (*sql.DB, error) {
 	var conErr error
-	c_string := "user:pass@tcp(10.72.94.119:3305)/db"
+	c_string := "user:pass@tcp(10.72.94.119:3306)/db"
 	db, conErr = sql.Open("mysql", c_string)
 	if conErr != nil {
 		return db, conErr
@@ -24,28 +34,31 @@ func Connect() (*sql.DB, error) {
 }
 
 func Test() string {
-	_, err := Connect()
+	albums, err := SearchByArtist("John Coltrane")
 	if err != nil {
-		return err.Error()
+		log.Fatal(err)
 	}
-	return "succeeded"
+	return albums[0].Title + albums[0].Artist + strconv.Itoa(int(albums[0].ID))
 }
 
-func Show(name string) ([]Album, error) {
+func SearchByArtist(name string) ([]Album, error) {
+	var albums []Album
+	db, _ = Connect()
+	defer db.Close()
 	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
 	if err != nil {
-        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-    }
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
 	defer rows.Close()
 	for rows.Next() {
-        var alb Album
-        if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-            return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-        }
-        albums = append(albums, alb)
-    }
-    if err := rows.Err(); err != nil {
-        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-    }
-    return albums, nil
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
